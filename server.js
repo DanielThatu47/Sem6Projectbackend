c406bcf1d8c29ede167cc98032a18b29da928245
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-04-04 00:43:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load environment variables
 const express = require('express');
 const authRoutes = require('./authRoutes');
 const mongoose = require('mongoose');
-const classroomRoutes = require("./Routes/classroomRoutes")
+const classroomRoutes = require("./classroomRoutes")
 const app = express();
 const cors = require('cors');
 app.use(cors());
